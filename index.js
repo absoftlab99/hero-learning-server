b1b6dbc91ac953a9895e401d4fa394165c3637e0
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-10-25 18:55:08 +0600

    installed cors successfully

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/courseCategory.json');
+
+const course = require('./data/course.json');
+
+app.get('/', (req, res) =>{
+    res.send('Course API Running');
+});
+
+app.get('/categories', (req, res) =>{
+    res.send(categories);
+})
+
+app.get('/course', (req, res) =>{
+    res.send(news);
+})
+
+app.get('/category/:id',(req, res) =>{
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(course);
+    }
+    else{
+        const course_category = course.filter(course => course.category_id === id);
+        res.send(course_category);
+    }
+})
+
+app.listen(port, ()=>{
+    console.log('Hero Learning Server Running on Port:', port);
+})
