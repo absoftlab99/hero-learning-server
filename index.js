0282e1196f9d6a5d033c7c9323315e53da4556b8
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-10-27 14:39:56 +0600

    some data change

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/course/:id', (req, res) =>{
 
 app.get('/category/:id',(req, res) =>{
     const id = req.params.id;
-    if(id === 0){
+    if(id == 0){
         res.send(course);
     }else{
         const course_category = course.filter(course => course.category_id === id);
